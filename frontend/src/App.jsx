22f7a6866d8c41fe38bf76d61c480d913c078d41
srikriydv <srikri5462ydv@gmail.com>
2024-07-21 13:25:46 +0545

    Adding sections like popular seller, popular courses, popular catagories, latest courses and creating component for header for code management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,24 @@
 import { useState } from "react";
 import logo from "./asset/logo.avif";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
   const linkStyle = {
-    fontSize: '40px'
+    fontSize: "40px",
   };
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-success" >
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarTogglerDemo03"
-            aria-controls="navbarTogglerDemo03"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <a className="navbar-brand" style={linkStyle} href="#">
-            Python Market Place
-          </a>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-              <li className="nav-item active">
-                <a className="nav-link" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Categories
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Header />
       <main className="mt-4">
         <div className="container">
-          <h3 className="mb-4">Latest Product <a href="#" className="float-end btn btn-dark">View All Product <i className="fa-solid fa-arrow-right-long"></i></a></h3>
+          {/* Latest Product  */}
+          <h3 className="mb-4">
+            Latest Product{" "}
+            <a href="#" className="float-end btn btn-dark">
+              View All Product <i className="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </h3>
           <div className="row">
             {/* Product Box */}
             <div className="col-12 col-md-3 mb-4">
@@ -132,96 +108,226 @@ function App() {
               </div>
             </div>
             {/* Product Box End  */}
-            {/* Product Box */}
-            <div className="col-12 col-md-3 mb-4">
-              <div className="card">
-                <img src={logo} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h4 className="card-title">Product title</h4>
-                  <h5 className="card-title text-muted">Price: Rs. 500</h5>
-                </div>
-                <div className="card-footer">
-                  <button title="Add to Cart" className="btn btn-success">
-                    <i className="fa-solid fa-cart-shopping"></i>
-                  </button>
-                  <button
-                    title="Add to Wishlist"
-                    className="btn btn-danger ms-1"
-                  >
-                    <i className="fa fa-heart"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-            {/* Product Box End  */}
-            {/* Product Box */}
-            <div className="col-12 col-md-3 mb-4">
-              <div className="card">
-                <img src={logo} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h4 className="card-title">Product title</h4>
-                  <h5 className="card-title text-muted">Price: Rs. 500</h5>
-                </div>
-                <div className="card-footer">
-                  <button title="Add to Cart" className="btn btn-success">
-                    <i className="fa-solid fa-cart-shopping"></i>
-                  </button>
-                  <button
-                    title="Add to Wishlist"
-                    className="btn btn-danger ms-1"
-                  >
-                    <i className="fa fa-heart"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-            {/* Product Box End  */}
-            {/* Product Box */}
-            <div className="col-12 col-md-3 mb-4">
-              <div className="card">
-                <img src={logo} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h4 className="card-title">Product title</h4>
-                  <h5 className="card-title text-muted">Price: Rs. 500</h5>
-                </div>
-                <div className="card-footer">
-                  <button title="Add to Cart" className="btn btn-success">
-                    <i className="fa-solid fa-cart-shopping"></i>
-                  </button>
-                  <button
-                    title="Add to Wishlist"
-                    className="btn btn-danger ms-1"
-                  >
-                    <i className="fa fa-heart"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-            {/* Product Box End  */}
-            {/* Product Box */}
-            <div className="col-12 col-md-3 mb-4">
-              <div className="card">
-                <img src={logo} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h4 className="card-title">Product title</h4>
-                  <h5 className="card-title text-muted">Price: Rs. 500</h5>
-                </div>
-                <div className="card-footer">
-                  <button title="Add to Cart" className="btn btn-success">
-                    <i className="fa-solid fa-cart-shopping"></i>
-                  </button>
-                  <button
-                    title="Add to Wishlist"
-                    className="btn btn-danger ms-1"
-                  >
-                    <i className="fa fa-heart"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-            {/* Product Box End  */}
-
           </div>
+          {/* End Latest Product */}
+
+          {/* Popular Catagories  */}
+          <h3 className="mb-4">
+            Popular Catagories{" "}
+            <a href="#" className="float-end btn btn-dark">
+              View All Catagories{" "}
+              <i className="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </h3>
+          <div className="row">
+            {/* Catagory Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Catagory title</h4>
+                </div>
+                <div className="card-footer">Product Downloads: 2356</div>
+              </div>
+            </div>
+            {/* Catagory Box End  */}
+            {/* Catagory Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Catagory title</h4>
+                </div>
+                <div className="card-footer">Product Downloads: 2356</div>
+              </div>
+            </div>
+            {/* Catagory Box End  */}
+            {/* Catagory Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Catagory title</h4>
+                </div>
+                <div className="card-footer">Product Downloads: 2356</div>
+              </div>
+            </div>
+            {/* Catagory Box End  */}
+            {/* Catagory Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Catagory title</h4>
+                </div>
+                <div className="card-footer">Product Downloads: 2356</div>
+              </div>
+            </div>
+            {/* Catagory Box End  */}
+          </div>
+          {/* End Popular Catagories */}
+
+          {/* Popular Product  */}
+          <h3 className="mb-4">
+            Popular Product{" "}
+            <a href="#" className="float-end btn btn-dark">
+              View All Product <i className="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </h3>
+          <div className="row">
+            {/* Product Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Product title</h4>
+                  <h5 className="card-title text-muted">Price: Rs. 500</h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-success">
+                    <i className="fa-solid fa-cart-shopping"></i>
+                  </button>
+                  <button
+                    title="Add to Wishlist"
+                    className="btn btn-danger ms-1"
+                  >
+                    <i className="fa fa-heart"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* Product Box End  */}
+            {/* Product Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Product title</h4>
+                  <h5 className="card-title text-muted">Price: Rs. 500</h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-success">
+                    <i className="fa-solid fa-cart-shopping"></i>
+                  </button>
+                  <button
+                    title="Add to Wishlist"
+                    className="btn btn-danger ms-1"
+                  >
+                    <i className="fa fa-heart"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* Product Box End  */}
+            {/* Product Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Product title</h4>
+                  <h5 className="card-title text-muted">Price: Rs. 500</h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-success">
+                    <i className="fa-solid fa-cart-shopping"></i>
+                  </button>
+                  <button
+                    title="Add to Wishlist"
+                    className="btn btn-danger ms-1"
+                  >
+                    <i className="fa fa-heart"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* Product Box End  */}
+            {/* Product Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Product title</h4>
+                  <h5 className="card-title text-muted">Price: Rs. 500</h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-success">
+                    <i className="fa-solid fa-cart-shopping"></i>
+                  </button>
+                  <button
+                    title="Add to Wishlist"
+                    className="btn btn-danger ms-1"
+                  >
+                    <i className="fa fa-heart"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* Product Box End  */}
+          </div>
+          {/* End Popular Product */}
+
+          {/* Popular Seller  */}
+          <h3 className="mb-4">
+            Popular Sellers
+            <a href="#" className="float-end btn btn-dark">
+              View All Sellers <i className="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </h3>
+          <div className="row">
+            {/* Seller Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Seller title</h4>
+                </div>
+                <div className="card-footer">
+                  Catagories: <a href="#">Python</a> <a href="#">JavaScript</a>
+                </div>
+              </div>
+            </div>
+            {/* Seller Box End  */}
+            {/* Seller Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Seller title</h4>
+                </div>
+                <div className="card-footer">
+                  Catagories: <a href="#">Python</a>
+                </div>
+              </div>
+            </div>
+            {/* Seller Box End  */}
+            {/* Seller Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Seller title</h4>
+                </div>
+                <div className="card-footer">
+                  Catagories: <a href="#">JavaScript</a>
+                </div>
+              </div>
+            </div>
+            {/* Seller Box End  */}
+            {/* Seller Box */}
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Seller title</h4>
+                </div>
+                <div className="card-footer">
+                  Catagories: <a href="#">GoLang</a> <a href="#">Swift</a>
+                </div>
+              </div>
+            </div>
+            {/* Seller Box End  */}
+          </div>
+          {/* End Popular Catagories */}
         </div>
       </main>
     </>
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <div className="container">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <a className="navbar-brand" style={linkStyle} href="#">
+            Python Market Place
+          </a>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Categories
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
