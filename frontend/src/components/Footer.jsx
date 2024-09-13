e2a8b835076b335c2c9da9064da136bb1b456932
srikriydv <srikri5462ydv@gmail.com>
2024-09-13 18:01:10 +0545

    final push

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
                 <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top pt-3">
                     <div className="col-md-4 d-flex align-items-center">
                         <a href="/" className="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
-                            Python Market Place
+                            पाठ्यक्रम बजार
                         </a>
                         <span className="mb-3 mb-md-0">© 2024 Company, Inc</span>
                     </div>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,20 +6,20 @@ import { useContext } from "react";
 function Header() {
   const linkStyle = {
     fontSize: "60px",
-    color:'#3a9434',
+    color: '#3a9434',
     fontWeight: "bold",
   };
   const { checkCustomer, checkVendor } = useContext(UserContext);
 
-  const {cartData, setCartData} = useContext(CartContext);
-  if(cartData == null){
+  const { cartData, setCartData } = useContext(CartContext);
+  if (cartData == null) {
     var cartItems = 0;
-  }else{
+  } else {
     var cartItems = cartData.length;
   }
   return (
     <>
-    <style>
+      <style>
         {`
           .navbar-nav .nav-link, 
           .navbar-nav .dropdown-item {
@@ -41,7 +41,7 @@ function Header() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <Link className="navbar-brand" style={linkStyle} to="/">
-            <i className="fa-solid fa-shop" style={{ fontSize: '60px', color:'#3a9434' }}></i> पाठ्यक्रम बजार
+            <i className="fa-solid fa-shop" style={{ fontSize: '60px', color: '#3a9434' }}></i> पाठ्यक्रम बजार
           </Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
@@ -55,52 +55,53 @@ function Header() {
                   Categories
                 </Link>
               </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  My Account
-                </a>
-                <ul className="dropdown-menu">
-                  {checkCustomer != 'true' &&
-                    <>
-                      <li><Link className="dropdown-item" to="/customer/login">Login</Link></li>
-                      <li><Link className="dropdown-item" to="/customer/register">Register</Link></li>
-                    </>}
-                  {checkCustomer == 'true' &&
-                    <>
-                      <li><Link className="dropdown-item" to="/customer/dashboard">Dashboard</Link></li>
-                      <li><Link className="dropdown-item" to="/customer/logout">logout</Link></li>
-                    </>
-                  }
-                </ul>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Vender Panel
-                </a>
-                <ul className="dropdown-menu">
-                  {checkVendor != 'true' &&
-                    <>
-                      <li><Link className="dropdown-item" to="/seller/login">Login</Link></li>
-                      <li><Link className="dropdown-item" to="/seller/register">Register</Link></li>
-                    </>}
-                  {checkVendor == 'true' &&
-                    <>
-                      <li><Link className="dropdown-item" to="/seller/dashboard">Dashboard</Link></li>
-                      <li><Link className="dropdown-item" to="/seller/logout">logout</Link></li>
-                    </>
-                  }
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/checkout">
-                  New Orders (4)
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/checkout">
-                  My Cart ({cartItems})
-                </Link>
-              </li>
+              {!checkVendor &&
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    My Account
+                  </a>
+                  <ul className="dropdown-menu">
+                    {checkCustomer != 'true' &&
+                      <>
+                        <li><Link className="dropdown-item" to="/customer/login">Login</Link></li>
+                        <li><Link className="dropdown-item" to="/customer/register">Register</Link></li>
+                      </>}
+                    {checkCustomer == 'true' &&
+                      <>
+                        <li><Link className="dropdown-item" to="/customer/dashboard">Dashboard</Link></li>
+                        <li><Link className="dropdown-item" to="/customer/logout">logout</Link></li>
+                      </>
+                    }
+                  </ul>
+                </li>
+              }
+              {!checkCustomer &&
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Vender Panel
+                  </a>
+                  <ul className="dropdown-menu">
+                    {checkVendor != 'true' &&
+                      <>
+                        <li><Link className="dropdown-item" to="/seller/login">Login</Link></li>
+                        <li><Link className="dropdown-item" to="/seller/register">Register</Link></li>
+                      </>}
+                    {checkVendor == 'true' &&
+                      <>
+                        <li><Link className="dropdown-item" to="/seller/dashboard">Dashboard</Link></li>
+                        <li><Link className="dropdown-item" to="/seller/logout">logout</Link></li>
+                      </>
+                    }
+                  </ul>
+                </li>
+              }
+              {checkCustomer &&
+                <li className="nav-item">
+                  <Link className="nav-link" to="/checkout">
+                    My Cart ({cartItems})
+                  </Link>
+                </li>
+              }
             </ul>
           </div>
         </div>
